575bd1a4136b88f883910787a1b26910cd64859e
Voyager <chriscoonwilliam@gmail.com>
2019-12-14 12:42:37 -0800

    finished project, starting readme now

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -82,3 +82,33 @@ function promptUser() {
     });
 }
 
+function calcOrder(amiiboChoice, amiiboQuantity) {
+    connection.query('SELECT * FROM amiibo_list WHERE amiibo_id = ' + amiiboChoice, function (err, results) {
+        console.log(results[0]);
+        if (err) { console.log(err) };
+        if (amiiboQuantity <= results[0].store_quantity) {
+            const totalPrice = results[0].price * amiiboQuantity;
+            console.log("Congratulations! You are now the proud owner of " + amiiboQuantity + " Amiibo!")
+            console.log("Your total cost is: " + totalPrice);
+            connection.query("UPDATE amiibo_list SET store_quantity = store_quantity - " + amiiboQuantity + " WHERE amiibo_id = " + amiiboChoice);
+            displayShop();
+        }
+        else {
+            console.log("Sorry, we don't have enough amiibo to fill this order!")
+            inquirer.prompt([
+                {
+                    name: 'retry',
+                    type: 'confirm',
+                    message: 'Would try adopting again?'
+                }
+            ]).then(answers => {
+                if (answers.retry === true) {
+                    promptUser();
+                }
+                else {
+                    connection.end();
+                }
+            })
+        }
+    });
+}
